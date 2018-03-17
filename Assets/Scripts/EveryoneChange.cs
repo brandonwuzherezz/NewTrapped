03f20ac6ea4569d6f556617d459841f0bb6e6a26
brandonwuzherezz <bsamudio@ucsc.edu>
2018-03-16 17:10:30 -0700

    LibraryShadow

---
+++ Assets/Scripts/EveryoneChange.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EveryoneChange : MonoBehaviour {
+    public MeshRenderer mycurrentRenderer;
+    public MeshRenderer BasecurrentRenderer;
+    public Texture MynormalTexture;
+    public Texture normalTexture;
+    // Use this for initialization
+    void Start () {
+        mycurrentRenderer = this.GetComponent<MeshRenderer>();
+        BasecurrentRenderer = GameObject.Find("BaseShadowLibrary").GetComponent<MeshRenderer>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		if(BasecurrentRenderer.material.mainTexture == normalTexture)
+        {
+            mycurrentRenderer.material.mainTexture = MynormalTexture;
+        }
+	}
+}
