5c0a97a57047026fa68330c38e632425e0a00aa2
brandonwuzherezz <bsamudio@ucsc.edu>
2018-05-16 14:40:19 -0700

    Restart

---
+++ Assets/Scripts/restartLevel.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class restartLevel : MonoBehaviour
+{
+    string currentScene;
+    // Update is called once per frame
+    void Update()
+    {
+        currentScene = SceneManager.GetActiveScene().name;
+        if (Input.GetKeyDown(KeyCode.F1))
+        {
+            SceneManager.LoadScene(currentScene);
+        }
+    }
+}
