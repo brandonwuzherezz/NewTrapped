5a9c74270544dfa96cfe02d64866eea0bb8aefca
brandonwuzherezz <bsamudio@ucsc.edu>
2018-05-22 19:59:52 -0700

    Transparent Door

---
+++ Assets/Transparency.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Transparency : MonoBehaviour {
+    public Color col;
+    // Use this for initialization
+    void Start () {
+        col = this.GetComponent<MeshRenderer>().material.color;
+        col.a = 1f; 
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
