dc36827dddbe268e7494a5020b5a20cec2545946
brandonwuzherezz <bsamudio@ucsc.edu>
2018-05-25 17:24:36 -0700

    Fades

--- Assets/Scripts/Bedroom1ToHallway.cs
+++ Assets/Scripts/Bedroom1ToHallway.cs
@@ -6,10 +6,12 @@ using UnityEngine.SceneManagement;
 public class Bedroom1ToHallway : MonoBehaviour
 {
 
-    void OnTriggerEnter(Collider col)
+    IEnumerator OnTriggerEnter(Collider col)
     {
         if (col.gameObject.tag == "Player")
         {
+            float fadeTime = GameObject.Find("Fade").GetComponent<CatchThisFade>().BeginFade(1);
+            yield return new WaitForSeconds(fadeTime);
             SceneManager.LoadScene("secondFloorHallway");  //Don't know Jay's Hallway name
             LoadLevel.Bedroom1B = true;
         }
--- Assets/Scripts/Bedroom2ToBathroom.cs
+++ Assets/Scripts/Bedroom2ToBathroom.cs
@@ -6,10 +6,12 @@ using UnityEngine.SceneManagement;
 public class Bedroom2ToBathroom : MonoBehaviour
 {
 
-    void OnTriggerEnter(Collider col)
+    IEnumerator OnTriggerEnter(Collider col)
     {
         if (col.gameObject.tag == "Player")
         {
+            float fadeTime = GameObject.Find("Fade").GetComponent<CatchThisFade>().BeginFade(1);
+            yield return new WaitForSeconds(fadeTime);
             SceneManager.LoadScene("Bathroom");
             LoadLevel.Bedroom2 = true;
         }
