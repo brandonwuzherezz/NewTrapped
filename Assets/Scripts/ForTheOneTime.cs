84635f0486b82913aee028c76b2e5bcafb7199c3
brandonwuzherezz <bsamudio@ucsc.edu>
2018-06-06 19:12:40 -0700

    yuo

--- Assets/Scripts/ForTheOneTime.cs
+++ Assets/Scripts/ForTheOneTime.cs
@@ -9,29 +9,34 @@ public class ForTheOneTime : MonoBehaviour
     public Animator animator;
     void Start()
     {
+       
+        GameObject.Find("Girl").GetComponent<Player_Movement>().anim.SetInteger("State", 0);
+        GameObject.Find("Girl").GetComponent<Player_Movement>().audioSource.Stop();
+        GameObject.Find("Girl").GetComponent<Player_Movement>().enabled = false;
+        StartDialogue();
         if (DoThisForMe.DeadDialouge.Contains(DisccusionName))
         {
+            GameObject.Find("Girl").GetComponent<Player_Movement>().enabled = true;
+            GameObject.Find("Girl").GetComponent<Player_Movement>().anim.SetInteger("State", 0);
             Destroy(gameObject);
         }
-        StartDialogue();
     }
     // Update is called once per frame
     void Update()
     {
-        GameObject.Find("Girl").GetComponent<Player_Movement>().anim.SetInteger("State", 0);
-        GameObject.Find("Girl").GetComponent<Player_Movement>().audioSource.Stop();
-        //GameObject.Find("Girl").GetComponent<Player_Movement>().enabled = false;
+        
         
 
         if (Input.GetKeyDown(KeyCode.Return))
         {
             //Press Enter And Then Close Image Using Anim
-            GameObject.Find("Girl").GetComponent<Player_Movement>().anim.SetInteger("State", 0);
             GameObject.Find("Girl").GetComponent<Player_Movement>().enabled = true;
+            GameObject.Find("Girl").GetComponent<Player_Movement>().anim.SetInteger("State", 0);
             DoThisForMe.DeadDialouge.Add(DisccusionName);
             EndDialogue();
             StartCoroutine("Wait");
         }
+       
     }
     void StartDialogue()
     {
